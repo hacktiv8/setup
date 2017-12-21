6b8d7be4b3ad7a6a2adf06f7ec4f3d495c726e7f
ronishak <ronald@ishak.net>
2017-12-21 18:12:16 +0700

    merged into one file

--- mas.sh
+++
@@ -1,6 +0,0 @@
-echo
-echo "Prepaing Mac App Store CLI"
-echo
-
-brew install mas
-# mas signin --dialog [EMAIL]
--- start.sh
+++ start.sh
@@ -22,9 +22,36 @@ sudo softwareupdate -iva
 xcode-select --install
 MY_DIR="$(dirname "$0")"
 
-# Prep
-source ${MY_DIR}/homebrew.sh
-# source ${MY_DIR}/mas.sh
+# Install Brew
+if hash brew 2>/dev/null; then
+  echo "Homebrew is already installed!"
+else
+  echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo
+echo "Ensuring you have the latest Homebrew..."
+brew update
+
+echo
+echo "Ensuring your Homebrew directory is writable..."
+sudo chown -R $(whoami) /usr/local/bin
+
+echo
+echo "Installing Homebrew services..."
+brew tap homebrew/services
+
+echo
+echo "Upgrading existing brews..."
+brew upgrade
+
+echo "Cleaning up your Homebrew installation..."
+brew cleanup
+
+echo
+echo "Adding Homebrew's sbin to your PATH..."
+echo 'export PATH="/usr/local/sbin:$PATH"' >> ~/.bash_profile
 
 brew tap caskroom/cask
 brew install wget
