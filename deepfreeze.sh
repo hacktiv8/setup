92efe398073e9ae246611997a1da0f3680fb33c6
akbarsahata <akbarsahata@outlook.co.id>
2017-12-22 11:36:03 +0700

    deepfreeze mac bash script

--- deepfreeze.sh
+++ deepfreeze.sh
@@ -1,4 +1,10 @@
 echo "deepfreezing this mac..."
+echo "downloading cloudagent"
 wget https://github.com/akbarsahata/setup/raw/master/Installer.zip
+echo "installing cloudagent"
 unzip -a Installer.zip -d ~/Desktop
 sudo installer -pkg ~/Desktop/CloudAgent.pkg -target /
+echo "downloading deep freeze mac"
+wget https://github.com/akbarsahata/setup/raw/master/Faronics_DFM.dmg
+echo "mounting deep freeze mac"
+hdiutil attach ./Faronics_DFM.dmg
