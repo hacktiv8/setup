3b349ab329f52ad4ff6c51672fb8bc72c5bcd42a
akbarsahata <akbarsahata@outlook.co.id>
2017-12-22 11:37:20 +0700

    moving faronic_dfm to desktop

--- deepfreeze.sh
+++ deepfreeze.sh
@@ -6,5 +6,6 @@ unzip -a Installer.zip -d ~/Desktop
 sudo installer -pkg ~/Desktop/CloudAgent.pkg -target /
 echo "downloading deep freeze mac"
 wget https://github.com/akbarsahata/setup/raw/master/Faronics_DFM.dmg
+mv ./Faronics_DFM.dmg ~/Desktop
 echo "mounting deep freeze mac"
-hdiutil attach ./Faronics_DFM.dmg
+hdiutil attach ~/Desktop/Faronics_DFM.dmg
